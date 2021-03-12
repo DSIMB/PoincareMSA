3fa537246d2f60e909e06586929913800139a069
Anna Susmelj <klanna@Anita-MacBook-Pro.local>
2021-03-12 14:31:22 +0100

    added rotation and text on colored values

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import numpy as np
 from pplots import *
 import pandas as pd
 import pickle
+from model import poincare_translation
 
 class PoincareMSA:
     """
@@ -33,6 +34,7 @@ class PoincareMSA:
         self.labels = {}
         self.labels_palettes = {}
         self.n_proteins = len(self.emb)
+        self.rotations = {}
 
         if embedding.columns[-1] != 'pm2':
             self.labels['proteins'] = embedding[embedding.columns[-1]].values
@@ -41,6 +43,18 @@ class PoincareMSA:
         else:
             print('WARNING! Protein labels are not found! It can lead to errors\
              in downstream functions.')
+
+    def rotate(self, labels_name, root_name=0):
+        assert labels_name in self.labels.keys(), "Labels name not found"
+        labels = self.labels[labels_name]
+        idx_root = np.where(labels==root_name)[0]
+        if len(idx_root) == 0:
+            print('Root not found!')
+        else:
+            idx_root = idx_root[0]
+            print(f"Rotating the map with respect to: {root_name} (index = {idx_root}.")
+            print(-self.emb[idx_root, :])
+            self.rotations[root_name] = poincare_translation(-self.emb[idx_root, :], self.emb)
 
     def get_tree_colors(
         self, 
@@ -61,23 +75,25 @@ class PoincareMSA:
             colors = pickle.load(f)
         colors['000'] = 'root'
 
-        tree_levels = [colors[protein_id] for protein_id in pMSA.labels['proteins']]
+        tree_levels = [colors[protein_id] for protein_id in self.labels['proteins']]
         self.labels[colors_name] = np.array(tree_levels)
         self.labels_palettes[colors_name] = get_colors(list(np.unique(tree_levels)),\
             palette=palette, 
             palette_name=palette_name)
 
         # self.labels_palettes[-1] = '#bdbdbd'
-        self.labels_palettes[colors_name][-1] = '#000000'
+        self.labels_palettes[colors_name][-1] = '#bdbdbd'
         self.labels_palettes[colors_name]['root'] = '#000000'
 
-    def plot_dics(
+    def plot_disc(
         self, 
-        labels_name=None, 
+        labels_name=None,
+        labels_text_name=None, 
         file=None,
         postfix='',
         show_text=False,
         plot_legend=True,
+        rotation=None,
         figsize=(7, 7)):
 
         if labels_name is None:
@@ -92,9 +108,25 @@ class PoincareMSA:
                 labels = None
                 col_dict = None
 
+        if labels_text_name is None:
+            labels_text = None
+        else:
+            if labels_text_name in self.labels.keys():
+                labels_text = self.labels[labels_name]
+            else:
+                print("This category name doesn't exist.")
+                labels_text = None
+
+        if rotation is None:
+            emb = self.emb
+        else:
+            assert rotation in self.rotations.keys(), "Labels name not found"
+            emb = self.rotations[rotation]
+
         plot_embedding(
-        self.emb, 
+        emb, 
         labels,
+        labels_text=labels_text,
         fig_width=figsize[0],
         fig_height=figsize[0],
         col_dict=col_dict,
@@ -102,7 +134,7 @@ class PoincareMSA:
         show_text=show_text,
         title='My glob example',
         circe_transparency=0.5,
-        file_name=f"{self.prefix}_{labels_name}{postfix}.png",
+        file_name=f"{self.prefix}_{labels_name}{rotation}{postfix}.png",
         is_hyperbolic=True,
         plot_legend=plot_legend
         )
@@ -112,14 +144,19 @@ if __name__ == "__main__":
     file_name = 'results/glob/PM3sigma=1.00gamma=1.00cosinepca=0_seed0.csv'
 
     pMSA = PoincareMSA(file_name)
+
+    pMSA.rotate('proteins', root_name=0)
+
     pMSA.get_tree_colors(
         'data/glob/glob_tree_cluster_1.pkl', 
         colors_name='glob_tree_cluster_1',        
         )
 
-    # pMSA.plot_dics('proteins')
-    pMSA.plot_dics('glob_tree_cluster_1',
+    pMSA.plot_disc('glob_tree_cluster_1',
+        labels_text_name='proteins',
         figsize=(12, 12),
+        show_text=False,
+        rotation=0,
         plot_legend=True)
 
 
