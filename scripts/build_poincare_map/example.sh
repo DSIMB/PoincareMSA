e06a3c129e50e2423ece528bc8c6691e9c57f8ee
Anna Klimovskaia Susmelj <anna@klimovsky.ru>
2021-10-07 16:31:34 +0200

    minor changes in plotting

--- scripts/build_poincare_map/example.sh
+++ scripts/build_poincare_map/example.sh
@@ -2,9 +2,10 @@
 
 # python ./main.py --plot true\
 #     --input_path /Users/klanna/UniParis/PoincareMSA/data/glob/Nfasta/\
-#     --output_path /Users/klanna/UniParis/results/glob/
-
+#     --output_path /Users/klanna/UniParis/results/glob/\
+#     --epochs 1500 
 
 python ./main.py --plot true\
     --input_path /Users/klanna/UniParis/PoincareMSA/examples/thioredoxins/fasta0.9/\
-    --output_path /Users/klanna/UniParis/results/thioredoxins/fasta0.9/
+    --output_path /Users/klanna/UniParis/results/thioredoxins/fasta0.9/\
+    --epochs 1500 
