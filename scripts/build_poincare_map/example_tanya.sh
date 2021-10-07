ee3441b33966bd82af945706a45f07e85eb78c2a
tatiana <tatiana.galochkina@inserm.fr>
2021-10-07 17:35:58 +0200

    root for thioredoxins

---
+++ scripts/build_poincare_map/example_tanya.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# python ./main.py --plot true\
+#     --input_path /Users/klanna/UniParis/PoincareMSA/data/glob/Nfasta/\
+#     --output_path /Users/klanna/UniParis/results/glob/
+
+
+python ./main.py --plot true\
+    --input_path /home/lavande/galochkina/SCIENCE/POINCARE/PoincareMSA/examples/thioredoxins/fasta0.9/\
+    --output_path /home/lavande/galochkina/SCIENCE/POINCARE/Thioredoxin/TEST/
+
+python ./main.py --plot true\
+    --input_path /home/lavande/galochkina/SCIENCE/POINCARE/PoincareMSA/examples/globins/fasta0.9/\
+    --output_path /home/lavande/galochkina/SCIENCE/POINCARE/Globins/TEST/
